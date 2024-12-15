86574aba6f175eb4bdfd790fbdc0689fed04ed9b
Jwala Yadav <jwalayadav141@gmail.com>
2024-12-15 11:37:55 +0530

    framework set

---
+++ Java_Interview_Question/src/main/java/Java_Interview_Question/Java_Interview_Question/App.java
@@ -0,0 +1,10 @@
+package Java_Interview_Question.Java_Interview_Question;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
